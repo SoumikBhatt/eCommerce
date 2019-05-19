1134c9ef05a90116881c6cd9b8786c9ea3455438
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-05-20 01:58:11 +0600

    login page UI done

--- app/src/main/java/com/soumik/e_commerce/MainActivity.kt
+++ app/src/main/java/com/soumik/e_commerce/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.soumik.e_commerce
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.soumik.e_commerce.activities.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            var intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
