be9b386708850a88acf49cddfc518da9abac7467
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-07-06 11:39:26 +0600

    productDetails updated

--- app/src/main/java/com/soumik/e_commerce/activities/ProductDetailsActivity.kt
+++ app/src/main/java/com/soumik/e_commerce/activities/ProductDetailsActivity.kt
@@ -1,12 +1,19 @@
 package com.soumik.e_commerce.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.database.*
 import com.soumik.e_commerce.R
 import com.soumik.e_commerce.models.Products
+import com.soumik.e_commerce.utils.showToast
 import kotlinx.android.synthetic.main.activity_product_details.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 class ProductDetailsActivity : AppCompatActivity() {
 
@@ -14,13 +21,59 @@ class ProductDetailsActivity : AppCompatActivity() {
         val PRODUCT_ID: String =  "PID"
     }
 
+    var productDetailsID = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
 
-        var productDetailsID = intent.getStringExtra(PRODUCT_ID)
+        productDetailsID = intent.getStringExtra(PRODUCT_ID)
 
         getProductDetails(productDetailsID)
+
+        fbtn_add_cart.setOnClickListener{
+            addingToCart()
+        }
+    }
+
+    private fun addingToCart() {
+        var saveCurrentTime = ""
+        var saveCurrentDate= ""
+
+        var calendar:Calendar = Calendar.getInstance()
+
+        var currentDate:SimpleDateFormat = SimpleDateFormat("MMM dd,yyyy")
+        saveCurrentDate = currentDate.format(calendar.time)
+
+
+        var currentTime:SimpleDateFormat = SimpleDateFormat("HH:mm:ss a")
+        saveCurrentTime = currentDate.format(calendar.time)
+
+        val cartListReference:DatabaseReference = FirebaseDatabase.getInstance().reference.child("Cart List")
+
+        val cartMap :HashMap<String,Any> = HashMap()
+
+        cartMap["pId"] = productDetailsID
+        cartMap["pName"] = tv_product_det_name.text.toString()
+        cartMap["price"] = tv_product_det_price.text.toString()
+        cartMap["date"] = saveCurrentDate
+        cartMap["time"] = saveCurrentTime
+        cartMap["quantity"] = ebtn_product_number.number
+        cartMap["discount"] = ""
+
+        cartListReference.child("User Cart View").child(LoginActivity.onlineUserPhone).child("Products").child(productDetailsID)
+            .updateChildren(cartMap).addOnCompleteListener { p0 ->
+                if (p0.isSuccessful){
+                    cartListReference.child("Admin Cart View").child(LoginActivity.onlineUserPhone).child("Products").child(productDetailsID)
+                        .updateChildren(cartMap).addOnCompleteListener { p0 ->
+                            if (p0.isSuccessful){
+                                showToast(applicationContext,"Added to CartList")
+                                startActivity(Intent(this@ProductDetailsActivity,HomeActivity::class.java))
+                                finish()
+                            }
+                        }
+                }
+            }
     }
 
     private fun getProductDetails(productDetailsID: String?) {
